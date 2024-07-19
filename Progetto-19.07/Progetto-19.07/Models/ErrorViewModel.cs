d5fa1b7940da2fe3e7a3be8620085c6f4f039e92
Luca-pintimalli <163635386+Luca-pintimalli@users.noreply.github.com>
2024-07-19 09:34:02 +0200

    Add files via upload
    Creazione Progetto + Istallazione pacchetti NuGet

---
+++ Progetto-19.07/Progetto-19.07/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Progetto_19._07.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
